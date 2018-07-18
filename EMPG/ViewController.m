d80f22c5021a488ff8bc4cf3cb84526177886849
Minh Le <lenguyetminh1998@gmail.com>
2018-07-18 11:17:57 -0700

    Update ViewController.m

--- EMPG/ViewController.m
+++ EMPG/ViewController.m
@@ -2,11 +2,13 @@
 #import "ViewController.h"
 #import "EPGCollectionViewCell.h"
 #import "EPGCollectionViewLayout.h"
+#import "DataModel.h"
 
 @interface ViewController (){
   UICollectionView *collectionView;
-  NSArray *fakeTitles;
   NSArray *fakeDescrip;
+  NSArray *allTitles;
+  NSInteger sectionCount;
 }
 @end
 
@@ -14,11 +16,33 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  sectionCount = 0;
+  //[self.view setBackgroundColor:[UIColor whiteColor]];
+  //self.view.backgroundColor = [UIColor whiteColor];
+
+
   // Do any additional setup after loading the view, typically from a nib.
   UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
   self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-  fakeTitles = @[@"Title1", @"Title2", @"Title3"];
-  fakeDescrip = @[@"D1", @"D2", @"D3"];
+
+  // Set titles and description for the movies
+
+  // Create a list of stations
+  EPGRenderer *epg = [[EPGRenderer alloc]init];
+  epg.stations = [[NSMutableArray alloc] init];
+  NSArray *stationTitle = [NSArray arrayWithObjects: @"fox", @"kpix5", @"abc7", @"nbc11", @"thecw", nil];
+
+  // Create a list of arrays to fil out the information
+  NSArray *d1 = [NSArray arrayWithObjects: @"New Girl", @"The Mick", @"Big Bang Theory", nil];
+  NSArray *d2 = [NSArray arrayWithObjects: @"East TN South vs Furman", @"Postgame", @"The Late Show with Stephen Colbert", nil];
+  NSArray *d3 = [NSArray arrayWithObjects: @"The Gong Show", @"Battle of Network Stars", nil];
+  NSArray *d4 = [NSArray arrayWithObjects: @"I Want A Dog For Christmas, Charlie Brown", nil];
+  NSArray *d5 = [NSArray arrayWithObjects: @"Two And A Half Man", @"Howdie Mandel All-Star Comedy Gala", nil];
+  allTitles = [NSArray arrayWithObjects: d1, d2, d3, d4, d5, nil];
+
+  fakeDescrip = @[@"D1", @"D2", @"D3", @"D4"];
+
+  // Set Data Source and Delegate and Cell ID
   collectionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:layout];
   [collectionView setDataSource:self];
   [collectionView setDelegate: self];
@@ -27,35 +51,43 @@
   [self.view addSubview:collectionView];
 }
 
+// Return how many rows within UI Collection View
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
-  return 5;
+  NSInteger row = allTitles.count;
+  return row;
 }
 
+
+// Return how many collumns within UI Collection View
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-  return fakeTitles.count;
+  NSInteger column = [allTitles[section] count];
+  return column;
 }
 
+
+// For each cell in the index path, put information inside the cell
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
-  EPGCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"epgCell" forIndexPath:indexPath ];
-  
-  // [cell.contentView addSubview:cell.title];
-  [cell setup:fakeTitles[indexPath.item] withDescription:fakeDescrip[indexPath.item]];
-  cell.title.textColor = [UIColor blackColor];
-  cell.title.text = fakeTitles[indexPath.item];
-  //[cell.layer]
-//  [cell.title setFrame:CGRectMake(cell.frame.origin.x, cell.frame.origin.y,70,30)];
-//  [cell.contentView addSubview:cell.title];
-  NSLog(@"this is the title %f", cell.frame.origin.x);
-  NSLog(@"this is the title %f", cell.frame.origin.x);
-  
-  
+  EPGCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"epgCell" forIndexPath:indexPath];
+
+   // Set the title text and description
+  NSArray *station = allTitles[indexPath.section];
+  cell.title.text = station[indexPath.item];
+  cell.descriptionText.text = @"hi";
+
+  // Set up the cell
+  [cell setup:station[indexPath.item] withDescription:fakeDescrip[indexPath.item]];
+
   return cell;
 }
 
+
+// Set the size of the cell
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
-  return CGSizeMake(100, 50);
+  return CGSizeMake(200, 100);
 }
 
+
+// Set padding between the cell
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
   return UIEdgeInsetsMake(30, 0, 20, 0);
 }
@@ -67,3 +99,15 @@
 
 
 @end
+
+//  NSLog(@"the title itself %@, the title put on %@", fakeTitles[indexPath.item], cell.title.text);
+//  NSLog(@"In the View: new x: %f with title: %@", cell.title.frame.origin.x, cell.title.text);
+//[cell.layer]
+//  [cell.title setFrame:CGRectMake(cell.frame.origin.x, cell.frame.origin.y,70,30)];
+//  [cell.contentView addSubview:cell.title];
+//NSLog(@"this is the title %f", cell.frame.origin.x);
+//  NSLog(@"this is the title %f", cell.frame.origin.x);
+
+  // Set the title text and description
+  //  cell.title.text = fakeTitles[indexPath.item];
+  //  cell.descriptionText.text = fakeDescrip[indexPath.item];
