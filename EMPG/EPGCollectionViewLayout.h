db4a4b46a4e2943cd45292b0ffcf482a1c2a5eac
Emily C <someoneemily@users.noreply.github.com>
2018-07-26 12:07:16 -0700

    Update EPGCollectionViewLayout.h

--- EMPG/EPGCollectionViewLayout.h
+++ EMPG/EPGCollectionViewLayout.h
@@ -2,5 +2,7 @@
 #import <UIKit/UIKit.h>
 
 @interface EPGCollectionViewLayout : UICollectionViewFlowLayout
+extern CGFloat const kVerticalPadding = 50;
+extern CGFloat const kBorderPadding = 30;
 - (NSArray *)indexPathsOfChannelHeaderViews;
 @end
