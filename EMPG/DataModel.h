8c93955d2ae1984c6ddf4a843ccdde9b13ca42f0
Emily C <someoneemily@users.noreply.github.com>
2018-07-26 10:15:24 -0700

    Update DataModel.h

--- EMPG/DataModel.h
+++ EMPG/DataModel.h
@@ -2,23 +2,29 @@
 #import <UIKit/UIKit.h>
 
 @interface AiringRenderer : NSObject
-@property (nonatomic) NSString *airingTitle;;
-@property (nonatomic) NSDate *airingStartTime;
-@property (nonatomic) NSDate *airingEndTime;
+@property(nonatomic, nonnull, readonly) NSString *airingTitle;
+@property(nonatomic, nonnull, readonly) NSDate *airingStartTime;
+@property(nonatomic, nonnull, readonly) NSDate *airingEndTime;
+- (void)setAiringTitle:(NSString *)titleValue;
+- (void)setAiringStartTime:(NSDate *)startTimeValue;
+- (void)setAiringEndTime:(NSDate *)endTimeValue;
 @end
-
-
 // For each station, create a list of airings
 @interface StationRenderer : NSObject
-@property (nonatomic) NSMutableArray <AiringRenderer*> *airings;
-@property (nonatomic) NSString *stationName;
-@property (nonatomic) UIImage *networkLogo;
+@property(nonatomic, nonnull, readonly) NSMutableArray<AiringRenderer *> *airings;
+@property(nonatomic, nonnull, readonly) NSString *stationName;
+@property(nonatomic, nonnull, readonly) UIImage *networkLogo;
+- (void)setAirings:(NSMutableArray<StationRenderer *> *)airingsArray;
+- (void)setStationName:(NSString *)stationNameValue;
 @end
-
-
 // Create a list of stations
 @interface EPGRenderer : NSObject
-@property (nonatomic) NSMutableArray<StationRenderer *> *stations;
-@property (nonatomic) long startTime;
-@property (nonatomic) long endTime;
+@property(nonatomic, nonnull, readonly) NSMutableArray<StationRenderer *> *stations;
+@property(nonatomic, nonnull, readonly) NSDate *startTime;
+@property(nonatomic, nonnull, readonly) NSDate *endTime;
+- (void)setStations:(NSMutableArray<StationRenderer *> *)stationsArray;
+@end
+// Create DataModel Class
+@interface DataModel : NSObject
++ (EPGRenderer *)createEPG;
 @end
