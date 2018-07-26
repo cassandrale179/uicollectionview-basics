eb8d57f6033a133f07af44cf90f24ff9bfa486df
Emily C <someoneemily@users.noreply.github.com>
2018-07-26 14:27:32 -0700

    Update StationCell.h

--- EMPG/StationCell.h
+++ EMPG/StationCell.h
@@ -1,6 +1,11 @@
 #import <UIKit/UIKit.h>
-
+/**
+ * A UICollectionReusableView representing a tv station.
+ */
 @interface StationCell : UICollectionReusableView
-@property (retain, nonatomic) IBOutlet UILabel *title;
--(void) setup: (NSString *) titleText;
+@property(retain, nonatomic) IBOutlet UILabel *title;
+/**
+ * The title displayed in the station cell.
+ */
+- (void)setup:(NSString *)titleText;
 @end
