44e654b8a9423281024693e43c116654e2561ecd
Emily C <someoneemily@users.noreply.github.com>
2018-08-01 13:28:40 -0700

    Update ViewController.h

--- EMPG/ViewController.h
+++ EMPG/ViewController.h
@@ -3,5 +3,8 @@
 /**
  * View controller class update the contents of the views in response changes in data
  */
-@interface ViewController : UIViewController <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+@protocol EPGDataSourceDelegate;
+@interface ViewController : UIViewController <EPGDataSourceDelegate, UICollectionViewDelegate,
+                                              UICollectionViewDataSource,
+                                              UICollectionViewDelegateFlowLayout>
 @end
