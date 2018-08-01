18f31338326cdccbfe004cfaa9c00671b90c5bb6
Emily C <someoneemily@users.noreply.github.com>
2018-08-01 13:29:33 -0700

    Add the delegate

--- EMPG/EPGCollectionViewLayout.h
+++ EMPG/EPGCollectionViewLayout.h
@@ -1,11 +1,19 @@
 #import <UIKit/UIKit.h>
-
 /**
  * UICollectionViewFlowLayout dictate how the cells will align
  * and what supplementary views will be shown.
  */
+@protocol EPGDataSourceDelegate;
 @interface EPGCollectionViewLayout : UICollectionViewFlowLayout
+@property (nonatomic, weak) id<EPGDataSourceDelegate> dataSource;
 extern CGFloat const kVerticalPadding = 50;
 extern CGFloat const kBorderPadding = 30;
 extern CGFloat const kAiringIntervalMinutes = 30;
+-(void)initWithDelegate:(id<EPGDataSourceDelegate>)dataSourceDelegate;
+@end
+@protocol EPGDataSourceDelegate <NSObject>
+-(NSDate *)layout:(EPGCollectionViewLayout *)epgLayout startTimeForItemAtIndexPath:(NSIndexPath *)indexPath;
+-(NSDate *)layout:(EPGCollectionViewLayout *)epgLayout EndTimeForItemAtIndexPath:(NSIndexPath *)indexPath;
+-(NSArray *)epgTimeArrayForLayout:(EPGCollectionViewLayout *)epgLayout;
+-(NSInteger) epgStationCountForLayout: (EPGCollectionViewLayout *) epgLayout;
 @end
