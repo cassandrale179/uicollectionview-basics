ab546b57de81ade44b74ecbab763d725f1cc7cd1
Emily C <someoneemily@users.noreply.github.com>
2018-08-06 16:02:54 -0700

    Update EPGCollectionViewLayout.h

--- EMPG/EPGCollectionViewLayout.h
+++ EMPG/EPGCollectionViewLayout.h
@@ -3,29 +3,24 @@
  * UICollectionViewFlowLayout dictate how the cells will align
  * and what supplementary views will be shown.
  */
-@protocol EPGDataSourceDelegate;
+@protocol EPGDataSource;
 @interface EPGCollectionViewLayout : UICollectionViewFlowLayout
-@property (nonatomic, weak) id<EPGDataSourceDelegate> dataSource;
-extern CGFloat const kVerticalPadding = 50;
+@property(nonatomic, weak) id<EPGDataSource> dataSource;
 extern CGFloat const kBorderPadding = 30;
 extern CGFloat const kAiringIntervalMinutes = 30;
--(void)initWithDelegate:(id<EPGDataSourceDelegate>)dataSourceDelegate;
--(CGFloat)numOfHalfHourIntervals:(NSDate *)airingStartTime withEndTime:(NSDate *)airingEndTime;
--(CGFloat) startingXPositionForAiring:(NSDate *)airingStartTime withIndexPath:(NSIndexPath *)indexPath;
+- (void)initWithDelegate:(id<EPGDataSource>)dataSourceDelegate;
 @end
-@protocol EPGDataSourceDelegate <NSObject>
-// Return start time for airing cell
--(NSDate *)layout:(EPGCollectionViewLayout *)epgLayout startTimeForItemAtIndexPath:(NSIndexPath *)indexPath;
-// Return end time for an airing cell
--(NSDate *)layout:(EPGCollectionViewLayout *)epgLayout EndTimeForItemAtIndexPath:(NSIndexPath *)indexPath;
-// Return start time for the entire epg (the first start time)
--(NSDate *)layoutStartTimeForEPG:(EPGCollectionViewLayout *)epgLayout;
-// Binary search for time
--(NSInteger)layoutBinarySearchForTime:(EPGCollectionViewLayout *)epgLayout forItemAtIndexPath:(NSIndexPath *)indexPath;
-// Return how many cells for hour header view
--(NSUInteger)epgTimeArrayCountForLayout:(EPGCollectionViewLayout *)epgLayout;
-// Return how many cells for station cell count
--(NSInteger)epgStationCountForLayout: (EPGCollectionViewLayout *) epgLayout;
-// Closest time interval before airing
--(NSTimeInterval)layoutTimeIntervalBeforeAiring:(EPGCollectionViewLayout *)epgLayout withClosestTimeIndex:(NSInteger)closestTimeIndex withAiringStartTime:(NSDate *)startTime;
+@protocol EPGDataSource <NSObject>
+- (NSDate *)layout:(EPGCollectionViewLayout *)epgLayout
+    startTimeForItemAtIndexPath:(NSIndexPath *)indexPath;
+- (NSDate *)layoutStartTimeForEPG:(EPGCollectionViewLayout *)epgLayout;
+- (NSInteger *)layoutBinarySearchForTime:(EPGCollectionViewLayout *)epgLayout
+                      forItemAtIndexPath:(NSIndexPath *)indexPath;
+- (NSDate *)layout:(EPGCollectionViewLayout *)epgLayout
+    EndTimeForItemAtIndexPath:(NSIndexPath *)indexPath;
+- (NSInteger *)epgTimeArrayCountForLayout:(EPGCollectionViewLayout *)epgLayout;
+- (NSInteger)epgStationCountForLayout:(EPGCollectionViewLayout *)epgLayout;
+- (NSTimeInterval)layoutTimeIntervalBeforeAiring:(EPGCollectionViewLayout *)epgLayout
+                            withClosestTimeIndex:(NSInteger)closestTimeIndex
+                             withAiringStartTime:(NSDate *)startTime;
 @end
